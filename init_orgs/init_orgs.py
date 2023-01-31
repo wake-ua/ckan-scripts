0abd2591f8763c37204c093d47d1c04a23817d29
espona <lucia.espona@gmail.com>
2023-01-31 17:59:03 +0100

    Refactoring

--- init_orgs/init_orgs.py
+++ init_orgs/init_orgs.py
@@ -6,9 +6,11 @@ import pprint
 import csv
 import shutil
 import os
+import sys
 
 # parameters from ENV
 from dotenv import load_dotenv
+
 load_dotenv('../.env')
 
 UPLOADS_PATH = os.getenv('GROUP_IMAGE_UPLOADS_PATH')
@@ -19,88 +21,118 @@ CKAN_URL = os.getenv('CKAN_URL')
 FILE_PATH = "./data/organizations_list.csv"
 IMAGE_DIR = "./data/image"
 
-CKAN_API_URL = "{}//api/3/action/".format(CKAN_URL)
+CKAN_API_URL = "{}/api/3/action/".format(CKAN_URL)
 CKAN_UPLOADS_URL = "{}/uploads/group/".format(CKAN_URL)
 
-# read the organizations file
-print(" - Read input file: {}".format(FILE_PATH))
 
-organizations = []
+def read_organizations(file_path: str) -> list:
+    # read the organizations file
+    print(" - Read input file: {}".format(file_path))
 
-with open(FILE_PATH) as csvfile:
+    organizations = []
 
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    with open(FILE_PATH) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
-    for row in reader:
-        organizations += [row]
-        print("\t * {}".format(row))
+        for row in reader:
+            organizations += [row]
+            print("\t * {}".format(row))
 
-print(" \t => Read {} organization(s): {}".format(len(organizations), ', '.join([org['name'] for org in organizations])))
+    print(" \t => Read {} organization(s): {}".format(len(organizations),
+                                                      ', '.join([org['name'] for org in organizations])))
 
-# prepare the data for import
-headers = {'Authorization': API_TOKEN}
+    return organizations
 
-for org in organizations:
-    print("\n * Create ORG: {}".format(org["name"]))
+
+def ckan_api_request(endpoint: str, method: str, token: str, data: dict = {}, params: dict = {}) -> (int, dict):
+    # set headers
+    headers = {'Authorization': token}
+
+    result = {}
+
+    # do the actual call
+    try:
+        if method == 'post':
+            response = requests.post('{}{}'.format(CKAN_API_URL, endpoint), data=data, params=params, headers=headers)
+        else:
+            response = requests.get('{}{}'.format(CKAN_API_URL, endpoint), params=params, headers=headers)
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+        result = response.json()
+        return 0, result
+
+    except HTTPError as http_err:
+        print(f'\t HTTP error occurred: {http_err} {response.json().get("error")}')  # Python 3.6
+        result = {"http_error": http_err, "error": response.json().get("error")}
+    except Exception as err:
+        print(f'\t Other error occurred: {err}')  # Python 3.6
+        result = {"error": err}
+
+    return -1, result
+
+
+def edit_organization(org: dict, update: bool = False) -> (int, dict):
+    # map attributes to ckan org
     ckan_org = {
-                    "name": org["name"],
-                    "display_name": org["title"],
-                    "description": org["description"],
-                    "image_display_url": "{}/{}".format(CKAN_UPLOADS_URL, org["image"]),
-                    "image_url": "{}".format(org["image"])
-                }
+        "name": org["name"],
+        "title": org["title"],
+        "description": org["description"],
+        "image_display_url": "{}/{}".format(CKAN_UPLOADS_URL, org["image"]),
+        "image_url": "{}".format(org["image"])
+    }
 
     # copy image to the uploads
     shutil.copyfile("{}/{}".format(IMAGE_DIR, org["image"]), "{}/{}".format(UPLOADS_PATH, org["image"]))
 
-    # do the import
-    try:
-        response = requests.post('{}organization_create'.format(CKAN_API_URL), data=ckan_org, headers=headers)
-
-        # If the response was successful, no Exception will be raised
-        response.raise_for_status()
-        organization_dict = response.json().get('result', {})
-        print("\t * Created: {}".format(organization_dict))
-
-    except HTTPError as http_err:
-        print(f'\t HTTP error occurred: {http_err} {response.json().get("error")}')  # Python 3.6
-    except Exception as err:
-        print(f'\t Other error occurred: {err}')  # Python 3.6
+    # call the endpoint
+    if not update:
+        success, result = ckan_api_request(endpoint="organization_create", method="post", token=API_TOKEN,
+                                           data=ckan_org)
     else:
-        print('\t Success!')
+        ckan_org["id"] = org["name"]
+        success, result = ckan_api_request(endpoint="organization_patch", method="post",
+                                           token=API_TOKEN, data=ckan_org)
+    return success, result
 
 
-# Make the HTTP request to retrieve all organizations.
-try:
-    response = requests.get('{}organization_list'.format(CKAN_API_URL), headers=headers)
+def main() -> int:
+    created_orgs = []
+    updated_orgs = []
 
-    # If the response was successful, no Exception will be raised
-    response.raise_for_status()
-    created_organizations_list = response.json().get('result', {})
-    print(" - Got total {} organizations: {}".format(len(created_organizations_list),
-                                                     ', '.join(created_organizations_list)))
+    # read the input file
+    organizations = read_organizations(FILE_PATH)
 
-except HTTPError as http_err:
-    print(f'\t HTTP error occurred: {http_err} {response.json().get("error")}')  # Python 3.6
-except Exception as err:
-    print(f'\t Other error occurred: {err}')  # Python 3.6
-else:
-    print('\t Success!')
+    # save the organizations
+    for org in organizations:
+        print("\n * Creating ORG: {}".format(org["name"]))
+        success, result = edit_organization(org)
+        if success >= 0:
+            print("\t * Created: {}".format(result))
+            created_orgs += [org["name"]]
+        else:
+            print("\t => Created Failed, trying UPDATE...")
+            success, result = edit_organization(org, update=True)
+            if success >= 0:
+                print("\t * Updated: {}".format(result))
+                updated_orgs += [org["name"]]
+            else:
+                print("\t => * Update Failed *")
+                return -1
 
-for org_name in created_organizations_list:
-    payload = {"id": org_name}
-    try:
-        response = requests.get('{}organization_show'.format(CKAN_API_URL), params=payload, headers=headers)
+    print(" * Finished: \n\t - Created {} organizations: {} "
+          "\n\t - Updated {} organizations: {}".format(len(created_orgs), ', '.join(created_orgs),
+                                                     len(updated_orgs), ', '.join(updated_orgs)))
 
-        # If the response was successful, no Exception will be raised
-        response.raise_for_status()
-        created_organization = response.json().get('result', {})
-        print("\t * Got organization {}: {}".format(created_organization["name"], created_organization))
-
-    except HTTPError as http_err:
-        print(f'\t HTTP error occurred: {http_err} {response.json().get("error")}')  # Python 3.6
-    except Exception as err:
-        print(f'\t Other error occurred: {err}')  # Python 3.6
+    success, total_orgs = ckan_api_request(endpoint="organization_list", method="get", token=API_TOKEN)
+    if success >= 0:
+        print("\nCKAN Organizations: {}".format(', '.join(total_orgs["result"])))
     else:
-        print('\t Success!')
+        print("\t => * Retrieving All Organizations Failed *")
+        return -1
 
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
