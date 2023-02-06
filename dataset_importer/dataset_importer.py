31b5657f3545bff430fc22bd455815d29383d0ac
espona <lucia.espona@gmail.com>
2023-02-06 13:30:49 +0100

    Resource preview upload

--- dataset_importer/dataset_importer.py
+++ dataset_importer/dataset_importer.py
@@ -8,6 +8,7 @@ import io
 import shutil
 import os
 import sys
+import datetime
 
 # parameters from ENV
 from dotenv import load_dotenv
@@ -19,6 +20,7 @@ CKAN_URL = os.getenv('CKAN_URL')
 
 # parameters
 FILE_DIR = "./data"
+TMP_DIR = os.path.join(FILE_DIR, 'tmp')
 PREVIEW_LINES = 10
 
 CKAN_API_URL = "{}/api/3/action/".format(CKAN_URL)
@@ -54,17 +56,21 @@ def read_datasets(file_dir: str) -> list:
     return datasets
 
 
-def ckan_api_request(endpoint: str, method: str, token: str, data: dict = {}, params: dict = {}) -> (int, dict):
+def ckan_api_request(endpoint: str, method: str, token: str, data: dict = {},
+                     params: dict = {}, files: list = [], dump: bool = True,
+                     content: str = 'application/json') -> (int, dict):
     # set headers
-    headers = {'Authorization': token,
-               'Content-Type': 'application/json'}
-
-    result = {}
+    headers = {'Authorization': token}
+    if content:
+        headers['Content-Type'] = content
 
     # do the actual call
     try:
         if method == 'post':
-            response = requests.post('{}{}'.format(CKAN_API_URL, endpoint), data=json.dumps(data), params=params, headers=headers)
+            if dump:
+                data = json.dumps(data)
+            response = requests.post('{}{}'.format(CKAN_API_URL, endpoint), data=data, params=params,
+                                     files=files, headers=headers)
         else:
             response = requests.get('{}{}'.format(CKAN_API_URL, endpoint), params=params, headers=headers)
 
@@ -83,6 +89,37 @@ def ckan_api_request(endpoint: str, method: str, token: str, data: dict = {}, pa
     return -1, result
 
 
+def handle_csv_resource(ckan_resource: dict) -> (str, dict):
+
+    file_name = ckan_resource["url"].split("/")[-1]
+    response = requests.get(ckan_resource["url"])
+    buff = io.StringIO(response.text)
+    cr = csv.DictReader(buff)
+    selected_lines = []
+    for row in cr:
+        selected_lines += [row]
+        if len(selected_lines) >= PREVIEW_LINES:
+            break
+
+    if selected_lines:
+        timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+        file_name = os.path.join(TMP_DIR, 'PREVIEW_' + timestamp + '_' + ckan_resource["url"].split("/")[-1])
+        header = [k for k in selected_lines[0].keys()]
+
+        print("\t  *  Saving preview at: " + file_name)
+        with open(file_name, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+
+            writer.writeheader()
+            for line in selected_lines:
+                writer.writerow(line)
+
+        ckan_resource["description"] += "\n\n Data header: " + ', '.join(header)
+        ckan_resource["description"] += "\n\n Full data available at: " + ckan_resource["url"]
+
+    return file_name, ckan_resource
+
+
 def edit_dataset(dataset: dict, update: bool = False) -> (int, dict):
 
     # map attributes to ckan dataset
@@ -93,7 +130,6 @@ def edit_dataset(dataset: dict, update: bool = False) -> (int, dict):
         "owner_org": dataset["organization"]["name"],
         "license_id": dataset["license_id"],
         "spatial": json.dumps(default_locations[dataset["organization"]["name"]])
-        # "extras": [{"key": "spatial", "value": json.dumps(default_locations[dataset["organization"]["name"]])}]
     }
 
     # check resources
@@ -106,22 +142,6 @@ def edit_dataset(dataset: dict, update: bool = False) -> (int, dict):
         ckan_resource["format"] = resource["format"]
         ckan_resource["size"] = resource["size"]
         ckan_resource["mimetype"] = resource["mimetype"]
-
-        if resource["url"].endswith(".csv"):
-            print("\t * CSV Resource found: {}".format(resource["url"].split("/")[-1]))
-            response = requests.get(resource["url"])
-            buff = io.StringIO(response.text)
-            cr = csv.DictReader(buff)
-            selected_lines = []
-            for row in cr:
-                selected_lines += [row]
-                if len(selected_lines) >= PREVIEW_LINES:
-                    break
-
-            if selected_lines:
-                header = [k for k in selected_lines[0].keys()]
-                ckan_resource["description"] += "\n Data header: " + ', '.join(header)
-
         ckan_resources += [ckan_resource]
 
     if ckan_resources:
@@ -138,6 +158,27 @@ def edit_dataset(dataset: dict, update: bool = False) -> (int, dict):
     return success, result
 
 
+def update_resource(resource: dict) -> (int, dict):
+
+    success = -1
+    result = {}
+
+    if resource["url"].endswith(".csv"):
+        file_name, ckan_resource = handle_csv_resource(resource)
+        if os.path.isfile(file_name):
+            # call the update resource endpoint
+            keys = ["description", "format", "id", "mimetype", "name", "package_id", "size"]
+            data = {k: ckan_resource[k] for k in keys}
+            success, result = ckan_api_request(endpoint="resource_create", method="post",
+                                               token=API_TOKEN, data=data,
+                                               files=[('upload', open(file_name, 'rb'))], dump=False,
+                                               content="")
+                                               # content="multipart/form-data")
+            os.remove(file_name)
+
+    return success, result
+
+
 def main() -> int:
     created_datasets = []
     updated_datasets = []
@@ -150,27 +191,41 @@ def main() -> int:
         print("\n * Creating DATA: {}".format(dataset["name"]))
         success, result = edit_dataset(dataset)
         if success >= 0:
-            print("\t * Created: {}".format(result))
+            print("\t * Created: {}...".format(str(result)[:500]))
             created_datasets += [dataset["name"]]
         else:
             print("\t => Created Failed, trying UPDATE...")
             success, result = edit_dataset(dataset, update=True)
             if success >= 0:
-                print("\t * Updated: {}".format(result))
+                print("\t * Updated: {}...".format(str(result)[:500]))
                 updated_datasets += [dataset["name"]]
             else:
                 print("\t => * Update Failed *")
                 return -1
 
-    print(" * Finished: \n\t - Created {} datasets: {} "
+        updated_dataset = result["result"]
+
+        # add previews to csv resources
+        for resource in updated_dataset["resources"]:
+            if resource["url"].endswith(".csv"):
+                print("\t * UPDATING csv resource {} (preview upload)...".format(resource['name']))
+                success, result = update_resource(resource)
+                if success >= 0:
+                    print("\t * Updated csv resource : {}...".format(str(result)[:500]))
+                else:
+                    print("\t => ERROR: * Resource csv pdate Failed *")
+                    return -1
+
+    print(" \t - Created {} datasets: {} "
           "\n\t - Updated {} datasets: {}".format(len(created_datasets), ', '.join(created_datasets),
                                                   len(updated_datasets), ', '.join(updated_datasets)))
 
     success, total_datasets = ckan_api_request(endpoint="package_list", method="get", token=API_TOKEN)
     if success >= 0:
-        print("\nCKAN Datasets ({}): {}".format(len(total_datasets["result"]), ', '.join(total_datasets["result"])))
+        print("\n - CKAN Datasets ({}): {}".format(len(total_datasets["result"]), ', '.join(total_datasets["result"])))
+
     else:
-        print("\t => * Retrieving All Datasets Failed *")
+        print("\t => * ERROR: Retrieving All Datasets Failed *")
         return -1
 
     return 0
