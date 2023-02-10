621d0199677108c21c97114f4c4ac1664abc9087
espona <lucia.espona@gmail.com>
2023-02-10 22:35:14 +0100

    Added transformation to include polygons as default location (srid transformation)

--- dataset_importer/dataset_importer.py
+++ dataset_importer/dataset_importer.py
@@ -9,6 +9,7 @@ import shutil
 import os
 import sys
 import datetime
+from django.contrib.gis.geos import Polygon
 
 # parameters from ENV
 from dotenv import load_dotenv
@@ -25,14 +26,55 @@ PREVIEW_LINES = 10
 
 CKAN_API_URL = "{}/api/3/action/".format(CKAN_URL)
 
-default_locations = {"ajuntament-alcoi": {"coordinates": [-0.47464877036321695, 38.69780773097776],
-                                          "type": "Point"
-                                          }
-                     }
+default_locations = {"ajuntament-alcoi": {"type": "Polygon", "coordinates": [
+    [[715092.573709193151444, 4290316.587757777422667], [715170.72468690527603, 4290343.026466369628906],
+     [715270.723552905838005, 4290623.96133420150727], [715912.748782854061574, 4290823.959395665675402],
+     [716218.761776866740547, 4291248.018417477607727], [716663.757870981353335, 4291208.018719019368291],
+     [717818.746982860495336, 4290724.897748008370399], [717811.715675718849525, 4290234.902394608594477],
+     [719567.714958137599751, 4289105.850134472362697], [719626.698830080451444, 4288895.85167841706425],
+     [720253.724041916429996, 4288538.980120005086064], [720417.706875170115381, 4288280.8571637282148],
+     [720949.655140671413392, 4288348.044171045534313], [721669.648473130888306, 4288109.921196753159165],
+     [721933.708361363038421, 4287223.991667378693819], [721810.663041123189032, 4286893.99486168846488],
+     [722280.658306433702819, 4286762.121187319979072], [722980.651871335343458, 4285284.009668275713921],
+     [722972.177640339010395, 4284847.850895561277866], [722947.683194867451675, 4283587.150343818590045],
+     [723574.629950621747412, 4282562.159780924208462], [723438.694041280657984, 4281704.042936975136399],
+     [723795.64369747065939, 4280849.988186108879745], [723814.627243071212433, 4279517.187713179737329],
+     [723088.696643540635705, 4278579.071709525771439], [722440.655489506316371, 4278365.01119989156723],
+     [721927.691555315512232, 4278580.946816941723228], [721070.668441049638204, 4279504.063655543141067],
+     [721099.652553774532862, 4279894.997122879140079], [720674.656580170732923, 4280159.057209450751543],
+     [720442.705802905606106, 4280018.12133218254894], [720438.721594000817277, 4280922.175436372868717],
+     [720290.676135121728294, 4281074.986589977517724], [719900.679532785201445, 4280602.178476817905903],
+     [719050.687429318204522, 4280114.99565287027508], [718971.703256563981995, 4278784.070630417205393],
+     [718035.696365443058312, 4277614.081206882372499], [715433.767680071759969, 4281755.918477905914187],
+     [715414.03119776409585, 4281756.802664711140096], [713731.752019644598477, 4281832.168120340444148],
+     [712406.764491775305942, 4281384.984973153099418], [712064.73631928418763, 4281649.982506167143583],
+     [710098.816766911186278, 4281612.170372801832855], [709750.773046669200994, 4280984.051546212285757],
+     [708920.780543712317012, 4280792.178349299356341], [708573.830538862966932, 4280904.989893683232367],
+     [708263.833850725903176, 4281488.109537940472364], [707654.777053611702286, 4281419.047593209892511],
+     [706815.800192240159959, 4282372.163757322356105], [706184.790543327224441, 4282064.979467236436903],
+     [705853.855741970706731, 4282123.103769154287875], [705807.840758358943276, 4282812.159814447164536],
+     [705777.840942375711165, 4283117.156939191743731], [706942.830535738612525, 4284174.959520334377885],
+     [707183.84418929903768, 4284699.954839564859867], [707270.796386934467591, 4285009.014633876271546],
+     [707954.774547579116188, 4285583.071334433741868], [709255.778209006763063, 4286224.003242642618716],
+     [710067.802354171290062, 4287208.993989516049623], [712905.744768728385679, 4288233.984090357087553],
+     [714274.717010502703488, 4290039.904398830607533], [715092.573709193151444, 4290316.587757777422667]]]}
+    }
+
+
+def transform_location(geometry: dict) -> dict:
+    new_location = {"type": geometry["type"], "coordinates": []}
+    # TODO: check for other geometry, this was only tested for Polygons
+    data = geometry["coordinates"][0]
+    poly_municipio = Polygon(data, srid=32630)
+    poly_gps = poly_municipio.transform(4326, clone=True)
+    coordinates = []
+    for c1, c2 in poly_gps.coords[0]:
+        coordinates = [[c1, c2]] + coordinates
+    new_location["coordinates"] = [coordinates]
+    return new_location
 
 
 def read_datasets(file_dir: str) -> list:
-
     # read the datasets files
     print(" - Read input dir: {}".format(file_dir))
 
@@ -44,7 +86,6 @@ def read_datasets(file_dir: str) -> list:
             print("\t * Read ", file_path)
 
             with open(file_path) as jsonfile:
-
                 dataset = json.load(jsonfile)["result"]
                 datasets += [dataset]
 
@@ -89,47 +130,43 @@ def ckan_api_request(endpoint: str, method: str, token: str, data: dict = {},
     return -1, result
 
 
-def handle_csv_resource(ckan_resource: dict) -> (str, dict):
-
-    file_name = ckan_resource["url"].split("/")[-1]
-    response = requests.get(ckan_resource["url"])
-    buff = io.StringIO(response.text)
-    cr = csv.DictReader(buff)
-    selected_lines = []
-    for row in cr:
-        selected_lines += [row]
-        if len(selected_lines) >= PREVIEW_LINES:
-            break
-
-    if selected_lines:
-        file_name = os.path.join(TMP_DIR, 'PREVIEW_' + ckan_resource["id"] + '_' + ckan_resource["url"].split("/")[-1])
-        header = [k for k in selected_lines[0].keys()]
-
-        print("\t  *  Saving preview at: " + file_name)
-        with open(file_name, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=header)
-
-            writer.writeheader()
-            for line in selected_lines:
-                writer.writerow(line)
-
-        ckan_resource["description"] += "\n\n Data header: " + ', '.join(header)
-        ckan_resource["description"] += "\n\n Full data available at: " + ckan_resource["url"]
-
-    return file_name, ckan_resource
+# def handle_csv_resource(ckan_resource: dict) -> (str, dict):
+#     file_name = ckan_resource["url"].split("/")[-1]
+#     response = requests.get(ckan_resource["url"])
+#     buff = io.StringIO(response.text)
+#     cr = csv.DictReader(buff)
+#     selected_lines = []
+#     for row in cr:
+#         selected_lines += [row]
+#         if len(selected_lines) >= PREVIEW_LINES:
+#             break
+#
+#     if selected_lines:
+#         file_name = os.path.join(TMP_DIR, 'PREVIEW_' + ckan_resource["id"]
+#         + '_' + ckan_resource["url"].split("/")[-1])
+#         header = [k for k in selected_lines[0].keys()]
+#
+#         print("\t  *  Saving preview at: " + file_name)
+#         with open(file_name, 'w', newline='') as csvfile:
+#             writer = csv.DictWriter(csvfile, fieldnames=header)
+#
+#             writer.writeheader()
+#             for line in selected_lines:
+#                 writer.writerow(line)
+#
+#         ckan_resource["description"] += "\n\n Data header: " + ', '.join(header)
+#         ckan_resource["description"] += "\n\n Full data available at: " + ckan_resource["url"]
+#
+#     return file_name, ckan_resource
 
 
 def edit_dataset(dataset: dict, update: bool = False) -> (int, dict):
-
     # map attributes to ckan dataset
-    ckan_dataset = {
-        "name": dataset["name"],
-        "title": dataset["title"]["es"],
-        "notes": dataset["notes"]["es"],
-        "owner_org": dataset["organization"]["name"],
-        "license_id": dataset["license_id"],
-        "spatial": json.dumps(default_locations[dataset["organization"]["name"]])
-    }
+    ckan_dataset = {"name": dataset["name"], "title": dataset["title"]["es"], "notes": dataset["notes"]["es"],
+                    "owner_org": dataset["organization"]["name"], "license_id": dataset["license_id"],
+                    "spatial": json.dumps(transform_location(default_locations[dataset["organization"]["name"]]))}
+
+    # spatial (Check for multi-polygon)
 
     # check resources
     ckan_resources = []
@@ -159,7 +196,6 @@ def edit_dataset(dataset: dict, update: bool = False) -> (int, dict):
 
 
 def update_resource(resource: dict) -> (int, dict):
-
     success = -1
     result = {}
 
@@ -173,7 +209,7 @@ def update_resource(resource: dict) -> (int, dict):
                                                token=API_TOKEN, data=data,
                                                files=[('upload', open(file_name, 'rb'))], dump=False,
                                                content="")
-                                               # content="multipart/form-data")
+            # content="multipart/form-data")
             os.remove(file_name)
 
     return success, result
@@ -203,18 +239,18 @@ def main() -> int:
                 print("\t => * Update Failed *")
                 return -1
 
-        updated_dataset = result["result"]
-
-        # add previews to csv resources
-        for resource in updated_dataset["resources"]:
-            if resource["url"].endswith(".csv"):
-                print("\t * UPDATING csv resource {} (preview upload)...".format(resource['name']))
-                success, result = update_resource(resource)
-                if success >= 0:
-                    print("\t * Updated csv resource : {}...".format(str(result)[:500]))
-                else:
-                    print("\t => ERROR: * Resource csv pdate Failed *")
-                    return -1
+        # updated_dataset = result["result"]
+        #
+        # # add previews to csv resources
+        # for resource in updated_dataset["resources"]:
+        #     if resource["url"].endswith(".csv"):
+        #         print("\t * UPDATING csv resource {} (preview upload)...".format(resource['name']))
+        #         success, result = update_resource(resource)
+        #         if success >= 0:
+        #             print("\t * Updated csv resource : {}...".format(str(result)[:500]))
+        #         else:
+        #             print("\t => ERROR: * Resource csv pdate Failed *")
+        #             return -1
 
     print(" \t - Created {} datasets: {} "
           "\n\t - Updated {} datasets: {}".format(len(created_datasets), ', '.join(created_datasets),
